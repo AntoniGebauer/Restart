7e0abd445cece0afa492065b53527b55764733e8
Antoni Gebauer <antoni.gebauer@comarch.com>
2023-10-31 17:11:17 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Antoni Gebauer on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
